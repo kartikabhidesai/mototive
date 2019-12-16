180b36fcb6dae1b6f9b8083e9170e6684ac64504
kartikdesai123 <kartikdesai123@gmail.com>
2019-12-16 21:04:24 +0530

    change

--- resources/views/backend/include/header.blade.php
+++ resources/views/backend/include/header.blade.php
@@ -7,7 +7,7 @@
     <meta name="keywords" content="">
     <meta name="author" content="Phoenixcoded" />
     <!-- Favicon icon -->
-    <link rel="icon" href="{{ url('public/backend/assets/images/favicon.ico') }}" type="image/x-icon">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ url('public/frontend/assets/images/fevicon.png') }}" sizes="16x16">
     <link href="{{ url('public/backend/assets/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css"/>
     <!-- vendor css -->
     <link rel="stylesheet" href="{{ url('public/backend/assets/css/style.css') }}">
